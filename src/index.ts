8169259f1c85d428df4d0d55044a8438f0f71440
kyng-cytro <johndibashi2001@gmail.com>
2024-01-13 12:59:52 +0100

    chore: init :tada:

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { program } from "./commander";
+
+program.parse();
---
+++ src/utils/ffmpeg.ts
@@ -0,0 +1,146 @@
+import fs from "fs";
+import path from "path";
+import ffmpeg from "fluent-ffmpeg";
+const pathToFfmpeg = require("ffmpeg-static");
+const pathToFfprobe = require("ffprobe-static");
+import { genOutputFileName, showMessage } from "./helpers";
+
+// Set bins
+ffmpeg.setFfmpegPath(pathToFfmpeg!);
+ffmpeg.setFfprobePath(pathToFfprobe.path);
+
+/**
+ * Trims a video into multiple pieces based on a specified duration.
+ *
+ * @param videoPath - The path to the input video.
+ */
+export const run = async (
+  videoPath: string,
+  outputPath: string,
+  trimDuration: number,
+  useFolder = true,
+) => {
+  try {
+    // Get the duration of the input video
+    const { duration } = await getDuration(videoPath);
+
+    // Divide the duration into segments
+    const trims = divideDuration(duration, trimDuration);
+
+    await showMessage(
+      `Splitting video into ${trims.length} pieces of ${trimDuration} seconds videos.`,
+      "info",
+    );
+
+    // Create promises for each trim operation
+    const promises = trims.map((trim, i) =>
+      trimVideoSegment(
+        i + 1,
+        trim,
+        videoPath,
+        outputPath,
+        trimDuration,
+        useFolder,
+      ),
+    );
+
+    // Wait for all trim promises to complete
+    await Promise.all(promises);
+
+    await showMessage(`Split completed.`, "success");
+  } catch (e: any) {
+    await showMessage(`Something went wrong. ${e.message}`, "error");
+    process.exit(1);
+  }
+};
+
+/**
+ * Trims a specific segment of a video.
+ *
+ * @param trimCount - The index of the trim segment.
+ * @param seekValue - The starting time to begin the trim.
+ * @param videoPath - The path to the input video.
+ * @param outputPath - The path to put the output video.
+ * @param preferredDuration - The preferred duration for each trimmed segment.
+ * @returns - A promise that resolves when the trimming is completed.
+ */
+const trimVideoSegment = (
+  trimCount: number,
+  seekValue: number,
+  videoPath: string,
+  outputPath: string,
+  trimDuration = 30,
+  useFolder = true,
+) => {
+  // Create folder if needed
+  if (!fs.existsSync(path.join(outputPath, "split-output")) && useFolder) {
+    fs.mkdirSync(path.join(outputPath, "split-output"), { recursive: true });
+  }
+
+  // Create file path
+  // TODO: use file name and extention
+  const outPath = useFolder
+    ? path.join(
+        outputPath,
+        "split-output",
+        genOutputFileName(videoPath, trimCount),
+      )
+    : path.join(outputPath, genOutputFileName(videoPath, trimCount));
+
+  return new Promise((resolve, reject) => {
+    ffmpeg(videoPath)
+      .output(outPath)
+      .seekInput(seekValue)
+      .duration(trimDuration)
+      .on("end", () => {
+        resolve({ outPath });
+      })
+      .on("error", async function (err) {
+        await showMessage(
+          `An error occured on trim ${trimCount}.\nmessage: ${err.message}.`,
+          "warn",
+        );
+        reject(new Error(err.message));
+      })
+      .run();
+  });
+};
+
+/**
+ * Retrieves the duration of a video file.
+ *
+ * @param videoPath - The path to the input video.
+ * @returns - A promise that resolves with the duration of the video.
+ */
+const getDuration = (videoPath: string): Promise<{ duration: number }> => {
+  return new Promise((resolve, reject) => {
+    ffmpeg(videoPath).ffprobe((err, data) => {
+      if (err) {
+        reject(new Error(err.message));
+        return;
+      }
+      const video = data.streams.find((s) => s.codec_type === "video");
+      if (!video) {
+        reject(new Error("No video stream found"));
+        return;
+      }
+      const duration = Number(data.format?.duration);
+      resolve({ duration });
+    });
+  });
+};
+
+/**
+ * Divides the duration of a video into segments based on a preferred duration.
+ *
+ * @param videoDuration - The total duration of the video.
+ * @param preferredDuration - The preferred duration for each segment (default is 30 seconds).
+ * @returns - An array containing the start times for each segment.
+ */
+const divideDuration = (videoDuration: number, trimDuration = 30) => {
+  const result: number[] = [];
+  for (let i = 0; i < videoDuration; i += trimDuration) {
+    result.push(i);
+  }
+  return result;
+};
